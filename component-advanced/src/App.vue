0c09ab630aad0448b1978070bfbe8dc95cf54e2d
Jeff Kwok <jkwok97@gmail.com>
2020-01-29 10:34:19 -0700

    more advanced component topics

---
+++ component-advanced/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComp = 'app-quote'">Quote</button>
+                <button @click="selectedComp = 'app-author'">Author</button>
+                <button @click="selectedComp = 'app-new'">New</button>
+                <hr>
+                <p>{{ selectedComp }}</p>
+                <keep-alive>
+                    <component :is="selectedComp"></component>
+                </keep-alive>
+                <!--<app-quote>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p>A Wonderful Quote!</p>
+                    <p>A Wonderful Quote!</p>
+                    <p>A Wonderful Quote!</p>
+                </app-quote>-->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
+
+    export default {
+        data() {
+            return {
+                quoteTitle: 'The Quote',
+                selectedComp: 'app-quote'
+            }
+        },
+        components: {
+            'app-quote': Quote,
+            'app-author': Author,
+            'app-new': New
+        }
+    }
+</script>
+
+<style scoped>
+    h2 {
+        color: red;
+    }
+</style>
---
+++ vue-cli/src/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <app-server-status v-for="(server, index) in 5" :key="index"></app-server-status>
+    </div>
+</template>
+
+<script>
+    import serverStatus from './ServerStatus.vue'
+    export default {
+        components: {
+            'app-server-status': serverStatus 
+        }
+    }
+</script>
