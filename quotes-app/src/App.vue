e3a6ea8d7d75c7adcad4f7fd8dfb8bd063269c71
Jeff Kwok <jkwok97@gmail.com>
2020-01-30 10:22:59 -0700

    completes quote app

---
+++ quotes-app/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+        <app-header
+            :quoteCount="quotes.length"
+            :maxQuotes="maxQuotes"
+        >
+        </app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid 
+            :quotes="quotes"
+            @quoteDeleted="deletQuote"
+        ></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on a quote to delete
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+
+    export default {
+        data() {
+            return {
+                maxQuotes: 10,
+                quotes: ['Just to see something']
+            }
+        },
+        components: {
+            'app-quote-grid': QuoteGrid,
+            'app-new-quote': NewQuote,
+            'app-header': Header
+        },
+        methods: {
+            newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Max reached, Please delete quotes first')
+                }
+                this.quotes.push(quote);
+            },
+            deletQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
